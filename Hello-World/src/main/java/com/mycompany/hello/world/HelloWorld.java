0780d43300d5375987f68c7789f6d36a3579cac6
Eliasg35 <159221075+Eliasg35@users.noreply.github.com>
2024-02-08 21:14:20 -0600

    Add files via upload

---
+++ Hello-World/src/main/java/com/mycompany/hello/world/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hello.world;
+
+/**
+ *
+ * @author MNWEST
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
